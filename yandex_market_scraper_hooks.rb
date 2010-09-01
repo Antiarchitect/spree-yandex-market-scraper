2967b0e9130bf9d6dce0abd58bee65c9a5526c3e
Andrey Voronkov <voronkovaa@gmail.com>
2010-09-02 08:35:44 +1000

    fix for cart

--- yandex_market_scraper_hooks.rb
+++ yandex_market_scraper_hooks.rb
@@ -1,3 +1,4 @@
 class YandexMarketScraperHooks < Spree::ThemeSupport::HookListener
   replace :admin_product_form_left, 'admin/products/market_description'
+  replace :cart_item_description, 'orders/cart_item_description'
 end
